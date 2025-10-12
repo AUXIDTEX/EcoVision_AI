2676f3a2314fecaeed5d617dbdce69f2d48253ef
AUXIDTEX <133670051+AUXIDTEX@users.noreply.github.com>
2025-10-12 14:34:32 +0300

    optimized diff calculation

--- rewrite.py
+++ rewrite.py
@@ -756,54 +756,73 @@ class Grid_Analyzer(QLabel):
         self.treshold = 35
         self.target_label = target_label
 
+        self.grid_diffs = []
+
+    def calculate_grid(self):  
+
+        h, w, _ = self.img_arr.shape
+        self.square_size = math.gcd(w, h)
+        avg_img_color = tuple(np.mean(self.img_arr, axis=(0, 1)).astype(int))
+
+        self.grid_diffs.clear()
+
+        for y in range(0, h, self.square_size):
+            for x in range(0, w, self.square_size):
+                y_max = min(y + self.square_size, h)
+                x_max = min(x + self.square_size, w)
+                region = self.img_arr[y:y_max, x:x_max]
+
+                avg_square = tuple(np.mean(region, axis=(0, 1)).astype(int))
+                pol_dist = np.linalg.norm(np.array(avg_square) - np.array(avg_img_color))
+                diff = int((pol_dist / 441.67) * 100)
+
+                self.grid_diffs.append({
+                    "x": x, "y": y, "x_max": x_max, "y_max": y_max, "diff": diff
+                })
+
     def draw_grid(self, treshold):
         self.treshold = treshold
+
+        if not self.grid_diffs:
+            self.calculate_grid()
+
         self.update()
         
     def paintEvent(self, event):
         super().paintEvent(event)
-        if self.img_arr is None:
+        if not self.grid_diffs:
             return
-        
-        h, w, _ = self.img_arr.shape
-        square_size = math.gcd(w, h)
 
+        h, w, _ = self.img_arr.shape
         scale_x = self.width() / w
         scale_y = self.height() / h
 
-        avg_img_color = tuple(np.mean(self.img_arr, axis=(0, 1)).astype(int))
-
         painter = QPainter(self)
-        pen = QPen(QColor(0, 0, 0, 150)) 
+        pen = QPen(QColor(0, 0, 0, 100))
         pen.setWidth(1)
         painter.setPen(pen)
 
-        for y in range(0, h, square_size):
-            for x in range(0, w, square_size):
-                y_max = min(y + square_size, h)
-                x_max = min(x + square_size, w)
-                region = self.img_arr[y:y_max, x:x_max]
+        for square in self.grid_diffs:
+            x, y = square["x"], square["y"]
+            x_max, y_max = square["x_max"], square["y_max"]
+            diff = square["diff"]
 
-                avg_square = tuple(np.mean(region, axis=(0, 1)).astype(int))
-                pol_dist = np.linalg.norm(np.array(avg_square) - np.array((avg_img_color)))
-                diff = int((pol_dist / 441.67) * 100)  # Normalize to percentage (0-100)
+            if diff > self.treshold:
+                brush = QColor(255, 0, 0, 100)
+                painter.fillRect(
+                    int(x * scale_x),
+                    int(y * scale_y),
+                    int((x_max - x) * scale_x),
+                    int((y_max - y) * scale_y),
+                    brush
+                )
 
-                if diff > self.treshold:
-                    brush = QColor(255, 0, 0, 100)  # Red with some transparency
-                    painter.fillRect(
-                        int(x * scale_x), 
-                        int(y * scale_y), 
-                        int((x_max - x) * scale_x), 
-                        int((y_max - y) * scale_y), 
-                        brush
-                    )
-                
-                painter.drawRect(
-                    int(x * scale_x), 
-                    int(y * scale_y), 
-                    int((x_max - x) * scale_x), 
-                    int((y_max - y) * scale_y)
-                    )
+            painter.drawRect(
+                int(x * scale_x),
+                int(y * scale_y),
+                int((x_max - x) * scale_x),
+                int((y_max - y) * scale_y)
+            )
 
         painter.end()
 
